8bad9bca3f9b57517fb0990e23f2c40185530bbc
Will Usher <will.usher@openenergytransition.org>
2026-02-20 18:37:09 +0000

    Add start and end dates to the data model

--- src/op_importer/data_model.py
+++ src/op_importer/data_model.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from pydantic import BaseModel, Field, PositiveInt, field_serializer
 
 
@@ -7,19 +9,47 @@ class WorkPackage(BaseModel):
     project: PositiveInt | None = None
     work_package_type: PositiveInt = Field(..., serialization_alias="type")
     status: PositiveInt | None = None
+    startDate: datetime | None = None
+    dueDate: datetime | None = None
 
     @field_serializer("description", mode="plain")
     def serialize_description(self, value: str | None) -> dict | None:
-        return {"raw": value, "format": "markdown"}
+        if value:
+            return {"raw": value, "format": "markdown"}
+        else:
+            return {}
 
     @field_serializer("project", mode="plain")
     def serialize_project(self, value: PositiveInt) -> dict:
-        return {"href": f"/api/v3/projects/{value}"}
+        if value:
+            return {"href": f"/api/v3/projects/{value}"}
+        else:
+            return {}
 
     @field_serializer("work_package_type", mode="plain")
     def serialize_work_package_type(self, value: PositiveInt) -> dict:
-        return {"href": f"/api/v3/types/{value}"}
+        if value:
+            return {"href": f"/api/v3/types/{value}"}
+        else:
+            return {}
 
     @field_serializer("status", mode="plain")
     def serialize_status(self, value: PositiveInt) -> dict:
-        return {"href": f"/api/v3/statuses/{value}"}
+        if value:
+            return {"href": f"/api/v3/statuses/{value}"}
+        else:
+            return {}
+
+    @field_serializer("startDate", mode="plain")
+    def serialize_start_date(self, value: datetime) -> str:
+        if value is not None:
+            return value.date().isoformat()
+        else:
+            return None
+
+    @field_serializer("dueDate", mode="plain")
+    def serialize_due_date(self, value: datetime) -> str:
+        if value is not None:
+            return value.date().isoformat()
+        else:
+            return None
--- tests/test_data_model.py
+++ tests/test_data_model.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from op_importer.data_model import WorkPackage
 
 
@@ -9,14 +11,40 @@ def test_work_package_serialization():
         project=3,
         work_package_type=3,
         status=1,
+        startDate=datetime(1982, 6, 13),
+        dueDate=datetime(2024, 6, 30),
     )
 
     expected_serialized = {
         "subject": "Test Work Package",
-        "description": {"raw": "This is a test work package", "format": "markdown"},
+        "description": {"raw": "This is a test work package", 
+                        "format": "markdown"},
         "project": {"href": "/api/v3/projects/3"},
         "type": {"href": "/api/v3/types/3"},
         "status": {"href": "/api/v3/statuses/1"},
+        "startDate": "1982-06-13",
+        "dueDate": "2024-06-30",
+    }
+
+    actual_serialized = work_package.model_dump(by_alias=True)
+    assert (
+        actual_serialized == expected_serialized
+    ), f"Expected {expected_serialized}, but got {actual_serialized}"
+
+
+def test_work_package_serialization_defaults():
+
+    work_package = WorkPackage(subject="Test Work Package", 
+                               work_package_type=3)
+
+    expected_serialized = {
+        "subject": "Test Work Package",
+        "description": {},
+        "project": {},
+        "type": {"href": "/api/v3/types/3"},
+        "status": {},
+        "startDate": None,
+        "dueDate": None,
     }
 
     actual_serialized = work_package.model_dump(by_alias=True)
